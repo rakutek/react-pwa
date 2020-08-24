b27e56794d47cdc3d4b5117091157675307b4097
rakutek <rakutek12@gmail.com>
2020-08-24 17:23:04 +0900

    rename docs

---
+++ docs/precache-manifest.52d52abbddb7c5718e99783708f840df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5172c3421bf0ba640536fb95bebc6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a96c42cb76af0d087bd2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de6c9583273a68e7586c",
+    "url": "/static/js/2.2ccb96de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ccb96de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a96c42cb76af0d087bd2",
+    "url": "/static/js/main.2072b4ab.chunk.js"
+  },
+  {
+    "revision": "1612dab30dd87dad723c",
+    "url": "/static/js/runtime-main.9fc5f041.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
